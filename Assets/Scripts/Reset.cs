499b239072e59fc3a7622f899cd4046b6fc32946
Teodor Plop <teodorplop@gmail.com>
2023-05-27 09:46:55 +0300

    More comments

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -19,10 +19,15 @@ public class Reset : MonoBehaviour
     /// </summary>
     private void PerformReset()
     {
+        // Reload the scene
+        // This will reset all changed GameObjects
         SceneManager.LoadScene("Main");
 
+        // Reset the number of moves
         Cell.counter = 0;
+        // Reset cells array
         Cell.cells = new int[9];
+        // Reset the last cell that was clicked
         Cell.lastCell = null;
     }
 }
